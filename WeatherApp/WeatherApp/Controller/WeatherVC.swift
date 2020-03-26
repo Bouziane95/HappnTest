ac1b118f5643e5e46975948e09f353a2e71ae18d
Bouziane <bey.bouziane@live.fr>
2020-03-26 21:11:52 +0100

    bug fixes in WeatherVC

--- WeatherApp/WeatherApp/Controller/WeatherVC.swift
+++ WeatherApp/WeatherApp/Controller/WeatherVC.swift
@@ -74,6 +74,12 @@ class WeatherVC: UIViewController {
                         let tempConvertedToCelsius = Int(tempKelvin! - 273.15)
                         self.mainTemp.text = "\(tempConvertedToCelsius)°"
                         self.mainWeatherStateImg.image = UIImage(named: self.weatherIcon.updateWeatherIcon(condition: (response?.list[0].weather[0].icon)!))
+                        self.feelsLike = response?.list[0].main.feels_like
+                        self.humidit = response?.list[0].main.humidity
+                        self.tempMin = response?.list[0].main.temp_min
+                        self.tempMax = response?.list[0].main.temp_max
+                        self.windSpeed = response?.list[0].wind.speed
+                        self.deg = response?.list[0].wind.deg
                         
                         for index in 1...4{
                             let tempKelvin = response?.list[index].main.temp
@@ -90,30 +96,20 @@ class WeatherVC: UIViewController {
                                 self.temp1.text = "\(tempConvertedToCelsius) °"
                                 self.weatherStateImg1.image = UIImage(named: self.weatherIcon.updateWeatherIcon(condition: (response?.list[index].weather[0].icon)!))
                                 self.date1.text = "\(local)"
-                                
                             } else if index == 2 {
                                 self.temp2.text = "\(tempConvertedToCelsius) °"
                                 self.weatherStateImg2.image = UIImage(named: self.weatherIcon.updateWeatherIcon(condition: (response?.list[index].weather[0].icon)!))
                                 self.date2.text = "\(local)"
-                                
                             } else if index == 3 {
                                 self.temp3.text = "\(tempConvertedToCelsius) °"
                                 self.weatherStateImg3.image = UIImage(named: self.weatherIcon.updateWeatherIcon(condition: (response?.list[index].weather[0].icon)!))
                                 self.date3.text = "\(local)"
-                                
                             } else if index == 4 {
                                 self.temp4.text = "\(tempConvertedToCelsius) °"
                                 self.weatherStateImg4.image = UIImage(named: self.weatherIcon.updateWeatherIcon(condition: (response?.list[index].weather[0].icon)!))
                                 self.date4.text = "\(local)"
                             }
                         }
-                        
-                        self.feelsLike = response?.list[0].main.feels_like
-                        self.humidit = response?.list[0].main.humidity
-                        self.tempMin = response?.list[0].main.temp_min
-                        self.tempMax = response?.list[0].main.temp_max
-                        self.windSpeed = response?.list[0].wind.speed
-                        self.deg = response?.list[0].wind.deg
                     }
                 } else {
                     print("error")
